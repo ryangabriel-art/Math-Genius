d9da679f2f3440064827affc9f2257a1173fb624
ryangabriel-art <ryan.gabriel@nave.org.br>
2025-10-20 11:39:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function you(){
+    window.open('https://www.youtube.com/results?search_query=aula+sobre+fun%C3%A7%C3%A3o+quadratica+ponto+de+maximo', '_blank');
+}
+
+function to(){
+  window.open('https://docs.google.com/forms/d/e/1FAIpQLScSCXvwHXQ8-sg8Ee_fr3qB4TU68_ZXvBmTnM8nY2f9XgSs7w/viewform?usp=header', '_blank')
+}
+
+
---
+++ index1.js
@@ -0,0 +1,39 @@
+function site_2() {
+  window.open("Videos.html");
+}
+
+const images = document.querySelectorAll('.image');
+let expandedImage = null;
+
+
+function crescer(event) {
+  const img = event.currentTarget;
+
+  if (expandedImage) return; 
+
+  img.classList.add('expanded');
+  expandedImage = img;
+
+
+  document.addEventListener('click', clickFora);
+  document.body.style.overflow = 'hidden';
+}
+
+
+function clickFora(event) {
+  if (!expandedImage) return;
+
+
+  if (!expandedImage.contains(event.target)) {
+    expandedImage.classList.remove('expanded');
+    expandedImage = null;
+
+    document.removeEventListener('click', clickFora);
+    document.body.style.overflow = ''; 
+  }
+}
+
+
+images.forEach(img => {
+  img.addEventListener('click', crescer);
+});
